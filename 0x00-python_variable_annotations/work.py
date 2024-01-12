210d239f87e0c2ccebe76ab92ca5b1b430bd7d66
Fikayomi31 <fikayoogundijo@gmail.com>
2024-01-12 14:59:27 +0000

    task 100

---
+++ 0x00-python_variable_annotations/work.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" Module documentation """
+from typing import Sequence, Any, Union
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """Func doc"""
+    if lst:
+        return lst[0]
+    else:
+        return None
+
+print(safe_first_element.__annotations__)
