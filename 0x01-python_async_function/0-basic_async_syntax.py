35afcc73f17dc6aa13054db404f504bb5cef038e
Fikayomi31 <fikayoogundijo@gmail.com>
2024-01-15 22:33:11 +0000

    random

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""asynchronous coroutine that takes intefer argument"""
+import asyncio
+import random
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """asyncio function that take an integer argument
+    Args:
+        max_delay - default value of 1
+    """
+    random_delay = random.uniform(0, max_delay)
+    await asyncio.sleep(random_delay)
+    return random_delay
