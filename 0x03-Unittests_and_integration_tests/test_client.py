e8400724f68e1d0f645643ecafb3501887cd7452
Fikayomi31 <fikayoogundijo@gmail.com>
2024-04-04 06:03:23 -0700

    6. More patching

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -40,12 +40,15 @@ class TestGithubOrgClient(unittest.TestCase):
     def test_public_repos(self, mock_json) -> None:
         """Testing pubic repo function
         """
-        with patch("client.GithubOrgClient._public_repos_url",
-                    new_callable=PropertyMock) as mock_repo_url:
-            mock_repo_url.return_value = (
-                "https://api.github.com/orgd/google/repos")
+        with patch(
+            "client.GithubOrgClient._public_repos_url",
+            new_callable=PropertyMock
+        ) as mock_public_repos_url:
+            mock_public_repos_url.return_value = (
+                "https://api.github.com/orgs/google/repos"
+            )
             github_org_client = GithubOrgClient("google")
             self.assertEqual(github_org_client.public_repos(),
-                             ['repo1', 'repo2'])
+                             ["repo1", "repo2"])
             mock_json.assert_called_once()
-            mock_repo_url.assert_called_once()
+            mock_public_repos_url.assert_called_once()
